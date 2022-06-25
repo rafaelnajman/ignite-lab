6befb42e03fdc1f9f2bb3bd72f5b286e8e2be1e8
rafaelnajman <rafael.najman@hotmail.com>
2022-06-26 00:56:42 +0300

    refactor for graphql-codegen

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -8,48 +8,19 @@ import {
 } from "phosphor-react";
 
 import "@vime/core/themes/default.css";
-import { gql, useQuery } from "@apollo/client";
-
-const GET_LESSON_BY_SLUG_QUERY = gql`
-  query GetLessonBySlug($slug: String) {
-    lesson(where: { slug: $slug }) {
-      title
-      videoId
-      description
-      teacher {
-        bio
-        name
-        avatarURL
-      }
-    }
-  }
-`;
-
-interface GetLessonBySlugResponse {
-  lesson: {
-    title: string;
-    videoId: string;
-    description: string;
-    teacher: {
-      bio: string;
-      name: string;
-      avatarURL: string;
-    };
-  };
-}
+import { useGetLessonBySlugQuery } from "../graphql/generated";
 
 interface VideoProps {
   lessonSlug: string;
 }
 
 export function Video(props: VideoProps) {
-  const { data } = useQuery<GetLessonBySlugResponse>(GET_LESSON_BY_SLUG_QUERY, {
+  const { data } = useGetLessonBySlugQuery({
     variables: {
       slug: props.lessonSlug,
     },
   });
-
-  if (!data) {
+  if (!data || !data.lesson) {
     return (
       <div className="flex-1">
         <p>Loading...</p>
@@ -78,21 +49,23 @@ export function Video(props: VideoProps) {
               {data.lesson.description}
             </p>
             {/* teacher informations */}
-            <div className="flex items-center gap-4 mt-6">
-              <img
-                src="https://github.com/rafaelnajman.png"
-                alt="Teacher Photo"
-                className="rounded-full w-16 h-16 border-2 border-blue-500"
-              />
-              <div className="leading-relaxed">
-                <strong className="font-bold text-2xl block">
-                  {data.lesson.teacher.name}
-                </strong>
-                <span className="text-gray-200 text-sm block ">
-                  {data.lesson.teacher.bio}
-                </span>
+            {data.lesson.teacher && (
+              <div className="flex items-center gap-4 mt-6">
+                <img
+                  src="https://github.com/rafaelnajman.png"
+                  alt="Teacher Photo"
+                  className="rounded-full w-16 h-16 border-2 border-blue-500"
+                />
+                <div className="leading-relaxed">
+                  <strong className="font-bold text-2xl block">
+                    {data.lesson.teacher.name}
+                  </strong>
+                  <span className="text-gray-200 text-sm block ">
+                    {data.lesson.teacher.bio}
+                  </span>
+                </div>
               </div>
-            </div>
+            )}
           </div>
           {/* 2 buttons in right */}
           <div className="flex flex-col gap-4 w-full max-w-[800px] xl:w-fit">
